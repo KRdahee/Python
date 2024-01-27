d5f4ba226947d4b2a4305ca45d2d81b3ea5075bf
DaHee <113000888+KRdahee@users.noreply.github.com>
2024-01-28 00:33:15 +0900

    Add files via upload

---
+++ Pythontest010.py
@@ -0,0 +1,21 @@
+"""
+
+시퀀스의 인덱싱과 슬라이싱
+인덱싱 : 시퀀스의 한 원소를 가져오는 것
+슬라이싱 : 시퀀스의 일부분을 가져오는 것
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[2]) #3
+print(my_list[1:4]) #[2, 3, 4]
+
+"""
+
+my_str = "impossible"
+
+my_list = ["Apple", "Banana", "Chamwae", "Durian"]
+
+var1 = my_list[2]
+
+var2 = my_str[2:]
+
+print(var1, var2)
---
+++ Pythontest011.py
@@ -0,0 +1,20 @@
+"""
+
+시퀀스의 길이와 멤버 조사하기
+길이 : len(시퀀스)
+멤버 조사 : 원소 in 시퀀스 (원소가 존재하면 True, 존재하지 않으면 False)
+
+my_str = "abc"
+
+print(len(my_str)) #3 - my_str의 길이
+print('d' in my_str) #False - 존재하지 않음
+
+"""
+
+my_str = "impossible"
+
+my_list =  ["Apple", "Banana", "Chamwae", "Durian"]
+
+var1 = (len(my_str))
+
+var2 = ('egg'in my_list)
