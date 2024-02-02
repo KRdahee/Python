384bb398bb64191f27e1aff3ae52864514d16c25
DaHee <113000888+KRdahee@users.noreply.github.com>
2024-02-03 00:31:15 +0900

    Add files via upload

---
+++ Pythontest022.py
@@ -0,0 +1,67 @@
+# Syntax for printing a string of text
+print("I love Python!")
+
+
+# Syntax for printing numeric values
+print(360)
+print(32*45)
+
+
+# Syntax for printing the value of a variable
+value = 8*6
+print(value)
+
+
+# Multiplication, division, addition, and subtraction
+print(3*8/2+5-1)
+
+# Exponents
+print(4**6) # Syntax means 4 to the power of 6
+print(4**2) # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root of a number
+
+# To calculate how many different possible combinations can be
+# formed using a set of "x" characters with each character in "x"
+# having "y" number of possible values, you will need to use an 
+# exponent for the calculation:
+x = 4
+y = 26
+print(y**x)
+
+
+# Assignment of values to the variables:
+years = 10
+weeks_in_a_year = 52
+# This variable is assigned an arithmetic calculation:
+weeks_in_a_decade = years * weeks_in_a_year
+# Prints the calculation stored in the "weeks_in_a_decade" variable:
+print(weeks_in_a_decade)
+
+"""
+
+Python을 포함한 모든 프로그래밍 언어로 코드를 작성할 때는 정확한 구문을 사용하는 것이 중요합니다.
+작은 오타라도 구문 오류가 발생할 수 있으며 자동화된 Python 코딩 퀴즈 채점기는 코드를 잘못된 것으로 표시합니다. 
+이는 철자, 대소문자, 구두점 등의 단일 오류로 인해 코드가 실패할 수 있다는 점에서 실제 코딩 오류를 반영합니다. 
+
+일반적인 구문 오류:
+
+철자 오류
+
+잘못된 들여쓰기
+
+누락되거나 잘못된 키 문자:
+
+괄호 유형 - ( 곡선 ), [ 사각형 ], { 곱슬 }
+
+인용 유형 - "straight-double" 또는 'straight-single', "curly-double" 또는 'curly-single'
+
+콜론과 같은 소개 문자 차단-:
+
+데이터 유형 불일치
+
+Python 예약어가 없거나, 잘못 사용되었거나, 잘못 배치되었습니다.
+
+잘못된 대소문자(대문자/소문자) 사용 - Python은 대소문자를 구분하는 언어입니다. 
+
+"""
