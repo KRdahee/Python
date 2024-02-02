0c64fd47735457f51822c565bbb5379cc405db39
DaHee <113000888+KRdahee@users.noreply.github.com>
2024-02-03 00:30:53 +0900

    Add files via upload

---
+++ 022_Python Glossary terms from course 1, module 1.py
@@ -0,0 +1,96 @@
+"""
+
+코스 1, 모듈 1의 용어 및 정의 
+자동화: 수동 단계를 자동으로 수행되는 단계로 바꾸는 프로세스
+
+클라이언트 측 스크립팅 언어: 주로 웹 프로그래밍용입니다. 스크립트는 웹 서버에서 최종 사용자의 인터넷 브라우저로 전송된 다음 브라우저에서 실행됩니다.
+
+코드 편집기: 구문 강조, 자동 들여쓰기, 오류 검사, 자동 완성 등의 기능을 제공하는 도구
+
+컴퓨터 프로그램: 컴퓨터가 의도한 목표를 달성하기 위해 따르는 단계별 지침 목록
+
+함수: 특정 작업을 수행하는 재사용 가능한 코드 블록
+
+IDE: 소프트웨어 개발을 위한 포괄적인 기능을 제공하는 소프트웨어 애플리케이션
+
+인터프리터(Interpreter): 코드를 읽고 실행하는 프로그램
+
+입력: 최종 사용자가 프로그램에 제공하는 정보
+
+논리 오류: 올바르게 실행되지 못하게 하는 코드 오류 
+
+기계어: 가장 낮은 수준의 컴퓨터 언어. 이진 코드(1과 0)로 컴퓨팅 기계와 직접 통신합니다.
+
+객체 지향 프로그래밍 언어: 대부분의 코딩 요소는 구성 가능한 속성을 가진 객체로 간주됩니다.
+
+출력: 기능이나 컴퓨터 프로그램에 의해 수행된 작업의 최종 결과
+
+플랫폼별 스크립팅 언어: 특정 플랫폼에서 시스템 관리자가 사용하는 언어 
+
+프로그래밍: 다양한 방식으로 동작하도록 프로그램을 작성하는 과정 
+
+프로그래밍 코드 : 컴퓨터 프로그래밍 언어를 사용하여 규칙에 따라 작성된 컴퓨터 지침 세트
+
+프로그래밍 언어: 컴퓨터 프로그램을 작성하기 위한 구문과 의미를 갖춘 언어
+
+Python: 범용 프로그래밍 언어 
+
+Python 인터프리터: Python 코드를 컴퓨터 명령어로 변환하여 Python 코드를 읽고 실행하는 프로그램
+
+스크립트: 특정 작업을 자동화하는 데 자주 사용됩니다.
+
+의미론: 인간과 컴퓨터 언어 모두에서 진술이나 단어 모음의 의도된 의미나 효과
+
+구문: 인간 언어와 컴퓨터 언어 모두에서 각 명령문이 구성되는 방식에 대한 규칙
+
+변수: 프로그래밍 코드에서 변경 가능한 값을 임시로 저장하는 데 사용됩니다.
+
+
+
+Automation: The process of replacing a manual step with one that happens automatically
+
+Client-side scripting language: Primarily for web programming; the scripts are transferred from a web server to the end-user’s internet browser, then executed in the browser
+
+Code editors: Tools to provide features, including syntax highlighting, automatic indentation, error checking, and autocompletion
+
+Computer program: A step-by-step list of instructions that a computer follows to reach an intended goal
+
+Functions: A reusable block of code that performs a specific task
+
+IDE: A software application that provides comprehensive facilities for software development
+
+Interpreter: The program that reads and executes code
+
+Input: Information that is provided to a program by the end user
+
+Logic errors: Errors in code that prevent it from running correctly 
+
+Machine language: Lowest-level computer language. It communicates directly with computing machines in binary code (ones and zeros)
+
+Object-oriented programming language: Most coding elements are considered to be objects with configurable properties
+
+Output: the end result of a task performed by a function or computer program
+
+Platform-specific scripting language: Language used by system administrators on those specific platforms 
+
+Programming: The process of writing a program to behave in different ways 
+
+Programming code: A set of written computer instructions, guided by rules, using a computer programming language
+
+Programming languages: Language with syntax and semantics to write computer programs
+
+Python: A general purpose programming language 
+
+Python interpreter: Program that reads and executes Python code by translating Python code into computer instructions
+
+Script: Often used to automate specific tasks
+
+Semantics: The intended meaning or effect of statements, or collections of words, in both human and computer languages
+
+Syntax: The rules for how each statements are constructed in both human and computer languages
+
+Variables: These are used to temporarily store changeable values in programming code
+
+
+
+"""
