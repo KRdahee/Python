38ccbb85a69ae4a6b287a0ab884577aba5473059
DaHee <113000888+KRdahee@users.noreply.github.com>
2024-02-10 05:51:53 +0900

    Add files via upload

---
+++ 025_Python Expressions and variables.py
@@ -0,0 +1,41 @@
+"""
+In this scenario, two friends are eating dinner at a restaurant. The bill comes in the amount of 47.28 dollars. The friends decide to split the bill evenly between them, after adding 15% tip for the service. Calculate the tip, the total amount to pay, and each friend's share, then output a message saying "Each person needs to pay: " followed by the resulting number.
+
+"""
+#고쳐야함
+
+bill = 47    # Assign "bill" variable with bill amount
+tip = bill / 15  # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total   # Divide "total" by number of friends dining
+print("Each person needs to pay: " + "share") # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
+
+"""
+This code is supposed to take two numbers, divide one by another so that the result is equal to 1, and display the result on the screen. Unfortunately, there is an error in the code. Find the error and fix it, so that the output is correct.
+
+"""
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
+
+"""
+Combine the variables to display the sentence "How do you like Python so far?" 
+"""
+
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+
+print(word1,word2,word3,word4,word5,word6,word7)
+
+
+
+#What do you call a combination of numbers, symbols, or other values that produce a result when evaluated?
+
+#An expression,An expression is a combination of values, variables, operators, and calls to functions.
