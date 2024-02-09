3ad22a94878514bbb13b0adf75aa2b5b6c20671b
DaHee <113000888+KRdahee@users.noreply.github.com>
2024-02-10 05:58:26 +0900

    Add files via upload

---
+++ 026_Python Review_Defining functions.py
@@ -0,0 +1,28 @@
+def greeting(name):
+    print("Welcome, " + name)
+    
+greeting("Kay")
+greeting("Cameron")
+
+"""
+Welcome, Kay
+Welcome, Cameron
+"""
+
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+    
+greeting("Blake", "Software engineering")
+greeting("Ellis", "Software engineering")
+
+
+"""
+Welcome, Blake
+You are part of Software engineering
+Welcome, Ellis
+You are part of Software engineering
+
+"""
+
---
+++ 027_Python Built-in functions.py
@@ -0,0 +1,54 @@
+print() 
+
+month = "September"
+print("Investigate failed login attempts during", month, "if more than", 100)
+
+
+
+
+type() 
+
+print(type("This is a string"))
+
+
+
+
+str()
+
+number = 12
+string_representation = str(number)
+print(string_representation)
+
+
+
+
+sorted()
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+#[2, 12, 14, 19, 22, 32, 57]
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+print(time_list)
+#[2, 12, 14, 19, 22, 32, 57]
+#[12, 2, 32, 19, 57, 22, 14]
+
+
+
+
+max() and min() 
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(min(time_list))
+print(max(time_list))
+
+
+"""
+주요 시사점
+내장 함수는 하나의 간단한 명령으로 작업을 수행할 수 있는 Python의 강력한 도구입니다. 
+print() 함수는 인수를 화면에 인쇄하고, type() 함수는 인수의 데이터 유형을 반환하며, 
+sorted()는 인수를 구성하고, min() 및 max() 함수는 최소값과 최대값을 반환합니다. 각각 반복 가능합니다.
+"""
+
+
+
